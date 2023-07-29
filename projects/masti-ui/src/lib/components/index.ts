0404df0f0f0482f50c81697ac542fa162904dd69
Akhlaque <akhlaquea01@gmail.com>
2023-07-29 23:52:13 +0530

    Update v 0.3

--- projects/masti-ui/src/lib/components/index.ts
+++ projects/masti-ui/src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export * from './masti-button/masti-button.component';
 export * from './masti-chips/masti-chips.component';
 // export other components as well
+export * from './components.module'; // Export the module
