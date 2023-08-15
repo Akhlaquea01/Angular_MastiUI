31d1294439e78146b7446cb0c951e72e049aefa8
Akhlaque <akhlaquea01@gmail.com>
2023-08-16 01:50:45 +0530

    Masti Button Implemented with Test cases written on IT

--- projects/masti-ui/src/lib/components/index.ts
+++ projects/masti-ui/src/lib/components/index.ts
@@ -2,3 +2,5 @@ export * from './masti-button/masti-button.component';
 export * from './masti-chips/masti-chips.component';
 // export other components as well
 export * from './components.module'; // Export the module
+
+export { colorType } from './enums';
--- projects/masti-ui/src/lib/components/masti-button/masti-button.component.ts
+++ projects/masti-ui/src/lib/components/masti-button/masti-button.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { colorType } from '../enums';
 
 @Component({
   selector: 'lib-masti-button',
   templateUrl: './masti-button.component.html',
-  styleUrls: ['./masti-button.component.css']
+  styleUrls: ['./masti-button.component.css'],
 })
 export class MastiButtonComponent {
-
+  @Input() displayText = 'Button';
+  @Input() btnType: colorType = colorType.Default;
+  @Input() isDisabled: boolean = false;
 }
